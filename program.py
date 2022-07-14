03296640f8a5fdbd3f935f053ed6f150da061db8
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2022-07-14 20:03:59 +0000

    Converted to TakeControl class

--- program.py
+++ program.py
@@ -4,34 +4,42 @@ os.environ["DISPLAY"] = ":0"
 import readchar
 import pyautogui
 
+class TakeControl:
 
-while True:
-    key = readchar.readkey()
+    class Config:
+        SMALL_MOUSE_MOVEMENT = 10
+        BIG_MOUSE_MOVEMENT = 100
 
-    # Slow cursor moving
-    if key == 'w': pyautogui.move(0, -10)
-    if key == 'a': pyautogui.move(-10, 0)
-    if key == 's': pyautogui.move(0, 10)
-    if key == 'd': pyautogui.move(10, 0)
+    def take_control():
+        while True:
+            key = readchar.readkey()
 
-    # Fast cursor moving
-    if key == 'W': pyautogui.move(0, -100)
-    if key == 'A': pyautogui.move(-100, 0)
-    if key == 'S': pyautogui.move(0, 100)
-    if key == 'D': pyautogui.move(100, 0)
+            # Slow cursor moving
+            if key == 'w': pyautogui.move(0, -TakeControl.Config.SMALL_MOUSE_MOVEMENT)
+            if key == 'a': pyautogui.move(-TakeControl.Config.SMALL_MOUSE_MOVEMENT, 0)
+            if key == 's': pyautogui.move(0, TakeControl.Config.SMALL_MOUSE_MOVEMENT)
+            if key == 'd': pyautogui.move(TakeControl.Config.SMALL_MOUSE_MOVEMENT, 0)
 
-    # Clicking
-    if key == 'j': pyautogui.click()
-    if key == 'l': pyautogui.rightClick()
-    if key == 'u': pyautogui.doubleClick()
+            # Fast cursor moving
+            if key == 'W': pyautogui.move(0, -TakeControl.Config.BIG_MOUSE_MOVEMENT)
+            if key == 'A': pyautogui.move(-TakeControl.Config.BIG_MOUSE_MOVEMENT, 0)
+            if key == 'S': pyautogui.move(0, TakeControl.Config.BIG_MOUSE_MOVEMENT)
+            if key == 'D': pyautogui.move(TakeControl.Config.BIG_MOUSE_MOVEMENT, 0)
 
-    # Draging
-    if key == 'o': pyautogui.mouseDown()
-    if key == 'p': pyautogui.mouseUp()
+            # Clicking
+            if key == 'j': pyautogui.click()
+            if key == 'l': pyautogui.rightClick()
+            if key == 'u': pyautogui.doubleClick()
 
-    # Scrolling
-    if key == 'i': pyautogui.scroll(10)
-    if key == 'k': pyautogui.scroll(-10)
+            # Draging
+            if key == 'o': pyautogui.mouseDown()
+            if key == 'p': pyautogui.mouseUp()
 
-    # Quiting
-    if key == 'q': break
+            # Scrolling
+            if key == 'i': pyautogui.scroll(10)
+            if key == 'k': pyautogui.scroll(-10)
+
+            # Quiting
+            if key == 'q': break
+
+TakeControl.take_control()
